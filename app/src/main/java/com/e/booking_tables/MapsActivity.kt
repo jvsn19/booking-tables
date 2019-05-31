bc69b1acd199e9a467809677d4d35edee71f9ed3
João Vasconcelos <jvsn19@gmail.com>
2019-05-31 09:21:39 -0300

    map replace current location

--- app/src/main/java/com/e/booking_tables/MapsActivity.kt
+++ app/src/main/java/com/e/booking_tables/MapsActivity.kt
@@ -1,8 +1,11 @@
 package com.e.booking_tables
 
+import android.content.pm.PackageManager
 import android.location.Location
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.app.ActivityCompat
+import android.widget.Toast
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
 
@@ -15,11 +18,15 @@ import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarkerClickListener {
-    override fun onMarkerClick(p0: Marker?): Boolean = false
+    companion object  {
+        private const val LOCATION_PERMISSION_REQUEST_CODE = 1
+    }
+
     private lateinit var mMap: GoogleMap
     private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private lateinit var userLocation: Location
 
-
+    override fun onMarkerClick(p0: Marker?): Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -44,14 +51,40 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarker
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
-
         mMap.uiSettings.isZoomControlsEnabled = true
         mMap.setOnMarkerClickListener(this)
 
+        setUpMap()
+
+    }
+
+    private fun setUpMap() {
+        if(ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)
+            != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION),
+                LOCATION_PERMISSION_REQUEST_CODE)
+            return
+        }
+        mMap.isMyLocationEnabled = true
+
+        fusedLocationClient.lastLocation.addOnSuccessListener { location ->
+            if(location != null) {
+                userLocation = location
+                val currentLatLng = LatLng(location.latitude, location.longitude)
+                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLatLng, 18.0f))
+            }
+            else {
+                displayToast("Zoom your map to see restaurants")
+            }
+        }
+    }
+
+    private fun displayToast(message: String) {
+        val duration = Toast.LENGTH_LONG
+        val toast = Toast.makeText(applicationContext, message, duration)
+
+        toast.show()
     }
 
 }
