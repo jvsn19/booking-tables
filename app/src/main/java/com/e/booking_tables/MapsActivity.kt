068d19720d036b19cf89e5c0620753b24344a111
João Vasconcelos <jvsn19@gmail.com>
2019-06-02 14:39:17 -0300

    get address from marker

--- app/src/main/java/com/e/booking_tables/MapsActivity.kt
+++ app/src/main/java/com/e/booking_tables/MapsActivity.kt
@@ -1,10 +1,13 @@
 package com.e.booking_tables
 
 import android.content.pm.PackageManager
+import android.location.Address
+import android.location.Geocoder
 import android.location.Location
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v4.app.ActivityCompat
+import android.util.Log
 import android.widget.Toast
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
@@ -16,6 +19,7 @@ import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
+import java.lang.Exception
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarkerClickListener {
     companion object  {
@@ -71,8 +75,9 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarker
         fusedLocationClient.lastLocation.addOnSuccessListener { location ->
             if(location != null) {
                 userLocation = location
-                val currentLatLng = LatLng(location.latitude, location.longitude)
-                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLatLng, 18.0f))
+                val currentLatLng = LatLng(userLocation.latitude, userLocation.longitude)
+                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLatLng, 17.0f))
+                placeMarker(currentLatLng)
             }
             else {
                 displayToast("Zoom your map to see restaurants")
@@ -85,6 +90,34 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarker
         val toast = Toast.makeText(applicationContext, message, duration)
 
         toast.show()
+    }
+
+    private fun placeMarker(location: LatLng) {
+        val markerOption = MarkerOptions().position(location)
+        val addressText = getAddress(location)
+        markerOption.title(addressText)
+        mMap.addMarker(markerOption)
+    }
+
+    private fun getAddress(latLng: LatLng): String {
+        val geocoder = Geocoder(this)
+        val addresses: List<Address>?
+        val address: Address?
+        var addressText = ""
+
+        try {
+            addresses = geocoder.getFromLocation(latLng.latitude, latLng.longitude, 1)
+            if(addresses != null && addresses.isNotEmpty()) {
+                address = addresses[0]
+                for(i in 0 until address.maxAddressLineIndex + 1) {
+                    addressText += if (i == 0) address.getAddressLine(i) else "\n" + address.getAddressLine(i)
+                }
+            }
+        } catch(e: Exception) {
+            Log.e("MapsActivity", e.localizedMessage)
+        }
+
+        return addressText
     }
 
 }
